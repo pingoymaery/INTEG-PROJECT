e3353ef01cd5f2b6512e276292f7b5cd1bd80739
Maery Pingoy <pingoymaery@gmail.com>
2026-04-22 13:14:08 +0800

    update

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/CARS')
+def cars():
+    return render_template('cars.html')
+
 if __name__=='__main__':
     app.run(debug='True')
